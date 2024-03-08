dd73b4c92e49aeb980a209b87811e9a9db46b2e9
david <davferdz@gmail.com>
2024-03-08 14:36:31 +0100

    s

--- network_spatial_coherence/config.py
+++ network_spatial_coherence/config.py
@@ -15,19 +15,19 @@ base = {
     "handle_all_subgraphs": False,
 }
 
-if base['proximity_mode'] != 'experimental':
-    # Settings specific to simulation scenarios
-    simulation = {
-        "num_points": 1000,
-        "intended_av_degree": 10,
-        'plot_original_image': True
-    }
 
-else:
-    # Settings specific to experimental scenarios
-    experiment = {
-        # pixelgen_example_graph.csv  #edge_list_nbead_0_filtering_march_8.csv
-        "edge_list_title": "edge_list_nbead_0_filtering_march_8.csv",  # edge_list_distance_150_filtering_goodindex_simon.csv, nbead_7_goodindex_simon.csv, edge_list_nbead_4_filtering.csv
-        "weighted": False,
-        "weight_threshold": 0,
-    }
+# Settings specific to simulation scenarios
+simulation = {
+    "num_points": 1000,
+    "intended_av_degree": 10,
+    'plot_original_image': True
+}
+
+
+# Settings specific to experimental scenarios
+experiment = {
+    # pixelgen_example_graph.csv  #edge_list_nbead_0_filtering_march_8.csv
+    "edge_list_title": "edge_list_nbead_0_filtering_march_8.csv",  # edge_list_distance_150_filtering_goodindex_simon.csv, nbead_7_goodindex_simon.csv, edge_list_nbead_4_filtering.csv
+    "weighted": False,
+    "weight_threshold": 0,
+}
